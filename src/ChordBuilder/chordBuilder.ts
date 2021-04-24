e5ff60b96582358e980982597a0552fdf46548a0
Phil White <phil.white@creditsafe.com>
2021-04-24 22:55:29 +0100

    New way to gen sequences

--- src/ChordBuilder/chordBuilder.ts
+++ src/ChordBuilder/chordBuilder.ts
@@ -108,10 +108,10 @@ const modePatterns: { [Property in Mode]: string } = {
 };
 
 export const buildScale = (mode: Mode, tonic: Note) => {
-  const swapTonic = swapEnharmonic(mode)[tonic];
-  if (swapTonic !== undefined) {
-    tonic = swapTonic;
-  }
+  // const swapTonic = swapEnharmonic(mode)[tonic];
+  // if (swapTonic !== undefined) {
+  //   tonic = swapTonic;
+  // }
   const pattern = modePatterns[mode];
   const NOTES = flatMajors.includes(tonic) ? CHROMATIC_FLATS : CHROMATIC_SHARPS;
   const intervals = pattern.split("").map((x) => (x === "T" ? 2 : 1));
@@ -135,7 +135,73 @@ type BuildSequenceOptions = {
   mode: Mode;
 };
 
+/* 
+  Convert numerical representation of a pitch to a note.
+  Always converts accidentals to flat.
+  e.g.
+    0 -> "C1"
+    12 -> "C2"
+    13 -> "Db2"
+*/
+export const toNote = (n: number) =>
+  CHROMATIC_FLATS[n % CHROMATIC_FLATS.length] +
+  Math.floor(n / CHROMATIC_FLATS.length + 1);
+
+export const toPitch = (n: string) =>
+  CHROMATIC_FLATS.findIndex((x) => n.startsWith(x)) +
+  CHROMATIC_FLATS.length * (Number(n[n.length - 1]) - 1);
+
+type BuildTriadProps = {
+  tonic: Note;
+  mode: Mode;
+  degree: Degree;
+  octave: number;
+};
+
+export const buildTriad = ({
+  tonic,
+  mode,
+  degree,
+  octave,
+}: BuildTriadProps) => {
+  const pattern = modePatterns[mode];
+  const intervals = pattern.split("").map((x) => (x === "T" ? 2 : 1));
+
+  let startPitch = toPitch(tonic + octave);
+  for (let i = 0; i < degree - 1; i++) {
+    startPitch += intervals[i];
+  }
+
+  const root = startPitch;
+  const third =
+    root +
+    intervals[(0 + degree - 1) % intervals.length] +
+    intervals[(1 + degree - 1) % intervals.length];
+  const fifth =
+    third +
+    intervals[(2 + degree - 1) % intervals.length] +
+    intervals[(3 + degree - 1) % intervals.length];
+
+  return [root, third, fifth];
+};
+
 export const buildSequence = ({
+  length = 3,
+  startOctave = 3,
+  tonic = "C",
+  degree = 1,
+  mode,
+}: BuildSequenceOptions) => {
+  const triad = buildTriad({ tonic, mode, degree, octave: startOctave });
+  const result = [];
+  for (let i = 0; i < length; i++) {
+    const octaveModifier: number = Math.floor(result.length / 3) * 12;
+    result.push(triad[i % triad.length] + octaveModifier);
+  }
+  return result.map(toNote);
+};
+
+export const buildSequence2 = ({
   length = 3,
   startOctave = 3,
   tonic = "C",
